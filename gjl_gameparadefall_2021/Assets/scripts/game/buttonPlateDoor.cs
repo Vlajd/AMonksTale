977d60b81f3b9748f369c79c3afcc42aa4608f25
Vlajd <boriskostadinov2005@gmail.com>
2021-11-20 10:29:50 +0100

    lvl1_visualUpgrade

--- gjl_gameparadefall_2021/Assets/scripts/game/buttonPlateDoor.cs
+++ gjl_gameparadefall_2021/Assets/scripts/game/buttonPlateDoor.cs
@@ -2,10 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+namespace UnityEngine.Experimental.Rendering.Universal {
+
 public class buttonPlateDoor : MonoBehaviour
 {
     [SerializeField] private Collider2D doorCollider;
     public GameObject buttonPlate;
+    [SerializeField] private ShadowCaster2D shadow;
 
     void Update () {
 
@@ -22,10 +25,13 @@ public class buttonPlateDoor : MonoBehaviour
     void openDoor () {
 
         doorCollider.enabled = false;
+        shadow.castsShadows = false;
     }
 
     void closeDoor () {
 
         doorCollider.enabled = true;
+        shadow.castsShadows = false;
     }
+}
 }
