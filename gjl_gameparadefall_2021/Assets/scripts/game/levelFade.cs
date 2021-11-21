274c166876025b45678da31dae74c1b2d3a34998
Vlajd <boriskostadinov2005@gmail.com>
2021-11-21 13:50:21 +0100

    Update levelFade.cs

--- gjl_gameparadefall_2021/Assets/scripts/game/levelFade.cs
+++ gjl_gameparadefall_2021/Assets/scripts/game/levelFade.cs
@@ -7,6 +7,7 @@ public class levelFade : MonoBehaviour
     [SerializeField] private SpriteRenderer[] spriteRenderer = new SpriteRenderer[4];
     private bool hasEntered = false;
     private Color color = new Color32(0, 0, 0, 2);
+    [SerializeField] private GameObject ghost;
 
     void Start () {
 
@@ -18,6 +19,28 @@ public class levelFade : MonoBehaviour
         if (other.gameObject.CompareTag("mainPlayer")) {
 
             hasEntered = true;
+        }
+        else if (other.gameObject.CompareTag("npcPlayer")) {
+
+            gameObject.GetComponent<Collider2D>().isTrigger = true;
+        }
+        else if (other.gameObject.CompareTag("ghostPlayer")) {
+
+            gameObject.GetComponent<Collider2D>().isTrigger = true;
+            ghost.GetComponent<Collider2D>().isTrigger = false;
+        }
+    }
+
+    void OnTriggerExit2D (Collider2D other) {
+
+        if (other.gameObject.CompareTag("npcPlayer")) {
+
+            gameObject.GetComponent<Collider2D>().isTrigger = false;
+        }
+        else if (other.gameObject.CompareTag("ghostPlayer")) {
+
+            gameObject.GetComponent<Collider2D>().isTrigger = false;
+            ghost.GetComponent<Collider2D>().isTrigger = true;
         }
     }
 
