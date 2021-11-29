96c7d0e060a0ec3959726271a89c19f0c48747bd
Vlajd <boriskostadinov2005@gmail.com>
2021-11-29 16:55:01 +0100

    2.3

--- gjl_gameparadefall_2021/Assets/scripts/game/boxController.cs
+++ gjl_gameparadefall_2021/Assets/scripts/game/boxController.cs
@@ -22,6 +22,7 @@ public class boxController : MonoBehaviour
     void OnTriggerEnter2D (Collider2D other) {
 
         if (other.tag == "mainPlayer") {
+
             s_player[0].GetComponent<playerMovement>().walkSpeed -= speedReduction;
             s_player[0].GetComponent<playerMovement>().sprintSpeed -= speedReduction * 2;
             s_player[0].GetComponent<playerMovement>().boxPush = true;
@@ -30,11 +31,14 @@ public class boxController : MonoBehaviour
         else if (other.tag == "npcPlayer") {
         
             for (int i = 0; i < ghost.GetComponent<playerGhostParenter>().m_NPCCount; i++) {
-                s_npc[i].GetComponent<playerNPCMovement>().walkSpeed -= speedReduction;
-                s_npc[i].GetComponent<playerNPCMovement>().sprintSpeed -= speedReduction * 2;
-                s_npc[i].GetComponent<playerNPCMovement>().boxPush = true;
-                s_npc[i].GetComponent<Animator>().SetBool("isBoxPushed", true);
 
+                if (Vector3.Distance(transform.position, s_npc[i].transform.position) < 5f) {
+
+                    s_npc[i].GetComponent<playerNPCMovement>().walkSpeed -= speedReduction;
+                    s_npc[i].GetComponent<playerNPCMovement>().sprintSpeed -= speedReduction * 2;
+                    s_npc[i].GetComponent<playerNPCMovement>().boxPush = true;
+                    s_npc[i].GetComponent<Animator>().SetBool("isBoxPushed", true);
+                }
             }
         }
     }
@@ -50,11 +54,14 @@ public class boxController : MonoBehaviour
         else if (other.tag == "npcPlayer") {
         
             for (int i = 0; i < ghost.GetComponent<playerGhostParenter>().m_NPCCount; i++) {
-                s_npc[i].GetComponent<playerNPCMovement>().walkSpeed += speedReduction;
-                s_npc[i].GetComponent<playerNPCMovement>().sprintSpeed += speedReduction * 2;
-                s_npc[i].GetComponent<playerNPCMovement>().boxPush = false;
-                s_npc[i].GetComponent<Animator>().SetBool("isBoxPushed", false);
 
+                if (Vector3.Distance(transform.position, s_npc[i].transform.position) < 5f) {
+
+                    s_npc[i].GetComponent<playerNPCMovement>().sprintSpeed += speedReduction * 2;
+                    s_npc[i].GetComponent<playerNPCMovement>().walkSpeed += speedReduction;
+                    s_npc[i].GetComponent<playerNPCMovement>().boxPush = false;
+                    s_npc[i].GetComponent<Animator>().SetBool("isBoxPushed", false);
+                }
             }
         }
     }
